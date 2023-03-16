95b8c65a87f12aeeff386a7073e56c69e26edfdd
KhanhTran297 <giakhanh2972002@gmail.com>
2023-03-16 22:08:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentservice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.intentservice;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        Intent intent = new Intent(this,MyService.class);
+        stopService(intent);
+    }
+
+    public void stopService(View view) {
+        Intent intent = new Intent(this,MyService.class);
+        stopService(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+}
